8952dc22a719613e9ad043cbdde65a6fef7b1c36
rmedard <00Derfine@5>
2018-04-12 01:41:18 +0200

    W10

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
 (function ($) {
     $('section#block-welcomeblock').addClass('well well-sm');
-    $('ul.menu--services-menu').addClass('nav-pills nav-stacked');
+    $('ul.services-menu').addClass('nav-pills nav-stacked');
 }(jQuery));
