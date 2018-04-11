7040e47377e3f58f0fdd4fc44e69a93362c48784
rmedard <00Derfine@5>
2018-04-12 01:52:20 +0200

    W10

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
 (function ($) {
-    $('section#block-welcomeblock').addClass('well well-sm');
+    $('section#block-welcomeblock').addClass('well well-md');
     $('ul.menu--services-menu').addClass('nav-pills nav-stacked');
 }(jQuery));
