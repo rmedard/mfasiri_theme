60f0a6d314ca20004a79bce1486e2b79da058c33
rmedard <00Derfine@5>
2018-04-23 17:43:34 +0200

    W10

--- js/custom.js
+++ js/custom.js
@@ -4,4 +4,5 @@
     $('div.carousel-inner > div.item > img').addClass('img-thumbnail');
     $('article.service > div.content > div.field--name-body').addClass('well well-sm');
     $('article.service > div.content > div.field--name-field-service-image').addClass('thumbnail');
+    $('section.block-mailchimp-signup').addClass('well well-sm');
 }(jQuery));
