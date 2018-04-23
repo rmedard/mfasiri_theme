230b51f213df9b155d5cab7358906c9d42684d0f
rmedard <00Derfine@5>
2018-04-23 17:05:09 +0200

    W10

--- js/custom.js
+++ js/custom.js
@@ -2,4 +2,5 @@
     $('section#block-welcomeblock').addClass('well well-md');
     $('ul.menu--services-menu').addClass('nav-pills nav-stacked');
     $('div.carousel-inner > div.item > img').addClass('img-thumbnail');
+    $('article.service > div.content > div.field--name-body').addClass('well well-sm');
 }(jQuery));
