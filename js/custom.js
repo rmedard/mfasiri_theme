cdfa7111f6b164962a594b9fddac7dc9cb785d2e
rmedard <00Derfine@5>
2018-05-13 16:46:27 +0200

    W10

--- js/custom.js
+++ js/custom.js
@@ -5,4 +5,6 @@
     $('article.service > div.content > div.field--name-body').addClass('well well-sm');
     $('article.service > div.content > div.field--name-field-service-image').addClass('thumbnail');
     $('section.block-mailchimp-signup').addClass('well well-sm');
+    $('div.field field--name-field-content-left').addClass('col-md-6');
+    $('div.field field--name-field-content-right').addClass('col-md-6');
 }(jQuery));
