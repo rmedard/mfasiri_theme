5dd123bfe62ba0cbd03932eae63c3095951617de
rmedard <00Derfine@5>
2018-09-21 19:48:45 +0200

    W10

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,14 @@
 (function ($) {
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.1';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+
     $('section#block-welcomeblock').addClass('well well-md');
     $('ul.menu--services-menu').addClass('nav-pills nav-stacked');
     $('div.carousel-inner > div.item > img').addClass('img-thumbnail');
