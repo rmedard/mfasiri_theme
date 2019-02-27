fe457cdd68c097daceb97d1b7daa180cff88b5de
rmedard <medard.rebero@gmail.com>
2019-02-27 19:07:27 +0100

    W10

--- js/custom.js
+++ js/custom.js
@@ -9,7 +9,7 @@
     $('div.field.field--name-field-content-right').addClass('col-md-6 well well-sm');
     $('section.mailchimp-signup-subscribe-form').addClass('well well-sm');
     $('section#block-socialmedialinks').addClass('well well-sm');
-    $('div#views-bootstrap-ibiganiro-page-tuganire > div.media').addClass('well well-sm');
+    $('div#views-bootstrap-ibiganiro-page-duhugurane > div.media').addClass('well well-sm');
     $('div#views-bootstrap-twihugure-page-twihugure > div.media').addClass('well well-sm');
     $('div.field--name-field-course-youtube-podcast').addClass('well well-sm');
 
